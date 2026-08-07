545ab2de268575e9768722a57192b55aa9f2fdfe
Om Parate <184147255+omparate7@users.noreply.github.com>
2026-08-07 14:38:17 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # method1 : traverse , store , reverse, build again time compx o(2n) space O(n) 
+
+        # method 2: O(n) reverse on the go use 3 pointers.
+
+        #method1 
+
+        temp = list()
+        curr = head
+        while(curr):
+            temp.append(curr.val)
+            curr = curr.next
+
+        temp.reverse()
+
+        curr = head
+        for i in temp:
+            curr.val = i
+            curr=curr.next
+
+
+        return head
+
+
+
+    
