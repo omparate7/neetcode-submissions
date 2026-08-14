5387cf54d0752420e413ab2a2ea20c7848bc1635
Om Parate <184147255+omparate7@users.noreply.github.com>
2026-08-14 10:06:18 +0530

    Add: copy-linked-list-with-random-pointer - submission-5

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-5.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def __init__(self):
+        self.map = {}
+
+
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if head is None:
+            self.map[head]=None
+            return None
+        if head in self.map: # what i want to check is if head exists in  map or not . # if the key exists then it is sure that the value also exists , because of the nature of our recursion func.
+            return self.map[head]
+        copy = Node(head.val)
+        self.map[head] = copy
+        copy.next= self.copyRandomList(head.next)
+        copy.random = self.map[head.random]
+
+        return copy
+    
+        
